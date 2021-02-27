a2d643ab251c66ed0b37d128bea95cc11d146445
xashes <xashes@outlook.com>
2021-02-27 22:17:45 +0800

    backup folders

---
+++ backup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+folders = ["configs", "data", "projects", "books", "plum", "fork"]
+
+user = "pi"
+host = "pizero"
+target = "~/backup"
+
+
+def backup():
+    for f in folders:
+        folder = f"~/{f}"
+        cmd = f"rsync -av {folder} {user}@{host}:{target}"
+        subprocess.run(cmd, shell=True)
+    else:
+        print(f"Backup complete for: {folders}")
+
+
+if __name__ == "__main__":
+    backup()
